ad9e0e9ace99b84bc7845ed1acdf1a1defceb00f
amirnsamimi <amirnsamimi@gmail.com>
2025-08-16 01:25:26 +0330

    doc template added with swagger and yamljs

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import helmet from "helmet";
 import cors from "cors"
 import healthRoutes from "./routes/health.routes.ts";
 import authRoutes from "./routes/v1/auth.routes.ts";
+import swaggerUi from "swagger-ui-express"
+import YAML from "yamljs"
 
 const app = express();
 
@@ -11,6 +13,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(helmet())
 app.use(cors())
 
+/* ---- DOCS ---- */
+const swaggerDocument = YAML.load("./docs/swagger.yaml");
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 /* ---- ROUTES ---- */
 app.use("/health", healthRoutes)
