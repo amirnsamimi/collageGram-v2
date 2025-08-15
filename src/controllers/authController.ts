cbae3c1e226087b1f4ea2a1b3eb662f6443ebcbf
amirnsamimi <amirnsamimi@gmail.com>
2025-08-15 17:14:46 +0330

    user authController added

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,12 +3,32 @@ import bcrypt from 'bcrypt';
 import {hashPassword} from "../utils/passwords.js";
 import {db} from "../db/connection.js"
 import {users} from "../db/schema.js";
+import type {insertUser} from "../db/schema.js";
+import {generateJwtToken} from "../utils/jwt.js";
 
-export const register = async (req: Request, res: Response) => {
+export const register = async (req: Request<any, any, insertUser>, res: Response) => {
     try {
-        const {email, username, password, firstName, lastName} = req.body
-        const hashedPassword = await hashPassword(password)
-        const [user] =
+
+        const hashedPassword = await hashPassword(req.body.password);
+
+        const [user] = await db.insert(users).values({
+            ...req.body,
+            password: hashedPassword
+        }).returning({
+            id: users.id,
+            email: users.email,
+            username: users.username,
+            createdAt: users.createdAt
+        })
+
+        //signing in
+        const token = generateJwtToken({id: user.id, email: user.email, username: user.username})
+        return res.status(201).json({
+            message: 'User created successfully.',
+            user,
+            token
+        })
+
     } catch (err) {
         console.log('Registration error', err)
         res.status(500).json({error: 'failed to create user'})
