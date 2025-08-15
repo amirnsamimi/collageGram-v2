4fc78b86468176ee8e7dac76045a40376b0a0d65
amirnsamimi <amirnsamimi@gmail.com>
2025-08-15 17:22:30 +0330

    validation add for auth route

--- src/routes/v1/auth.routes.ts
+++ src/routes/v1/auth.routes.ts
@@ -1,16 +1,15 @@
-import { Router } from 'express';
+import {Router} from 'express';
+import {register} from "../../controllers/authController.js";
+import {bodyValidation} from "../../middleware/validation.js";
+import {insertUserSchema } from "../../db/schema.js";
+
 const router = Router();
 
 
-
-
-
-router.post('/login', (req, res) => {
+router.post('/login', bodyValidation(insertUserSchema), (req, res) => {
 
 })
 
-router.post('/register', (req, res) => {
-
-})
+router.post('/register', register)
 
 export default router;
